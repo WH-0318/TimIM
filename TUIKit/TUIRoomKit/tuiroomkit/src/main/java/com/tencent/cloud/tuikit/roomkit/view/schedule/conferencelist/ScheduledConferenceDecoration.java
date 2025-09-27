6d68fef8d60d32c413cfa57013a2657914b1efd0
Freeman <honggang.ju@woohooart.com>
2025-09-27 15:35:43 +0800

    修改会议页面逻辑；

--- TUIKit/TUIRoomKit/tuiroomkit/src/main/java/com/tencent/cloud/tuikit/roomkit/view/schedule/conferencelist/ScheduledConferenceDecoration.java
+++ TUIKit/TUIRoomKit/tuiroomkit/src/main/java/com/tencent/cloud/tuikit/roomkit/view/schedule/conferencelist/ScheduledConferenceDecoration.java
@@ -17,27 +17,27 @@ public class ScheduledConferenceDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
-        if (parent.getAdapter() instanceof ScheduledConferenceAdapter) {
-            ScheduledConferenceAdapter adapter = (ScheduledConferenceAdapter) parent.getAdapter();
-            int firstPosition = ((LinearLayoutManager) parent.getLayoutManager()).findFirstVisibleItemPosition();
-
-            String itemHeader = adapter.getRoomInfoDateString(firstPosition);
-            View headerView = LayoutInflater.from(parent.getContext()).inflate(R.layout.tuiroomkit_scheduled_room_date, parent, false);
-            TextView headerTextView = headerView.findViewById(R.id.scheduled_room_date);
-            headerTextView.setText(itemHeader);
-
-            int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getWidth(), View.MeasureSpec.EXACTLY);
-            int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.UNSPECIFIED);
-            int childWidth = ViewGroup.getChildMeasureSpec(widthSpec,
-                    parent.getPaddingLeft() + parent.getPaddingRight(),
-                    headerView.getLayoutParams().width);
-            int childHeight = ViewGroup.getChildMeasureSpec(heightSpec,
-                    parent.getPaddingTop() + parent.getPaddingBottom(),
-                    headerView.getLayoutParams().height);
-            headerView.measure(childWidth, childHeight);
-            headerView.layout(0, 0, headerView.getMeasuredWidth(), headerView.getMeasuredHeight());
-            headerView.draw(c);
-        }
+//        if (parent.getAdapter() instanceof ScheduledConferenceAdapter) {
+//            ScheduledConferenceAdapter adapter = (ScheduledConferenceAdapter) parent.getAdapter();
+//            int firstPosition = ((LinearLayoutManager) parent.getLayoutManager()).findFirstVisibleItemPosition();
+//
+//            String itemHeader = adapter.getRoomInfoDateString(firstPosition);
+//            View headerView = LayoutInflater.from(parent.getContext()).inflate(R.layout.tuiroomkit_scheduled_room_date, parent, false);
+//            TextView headerTextView = headerView.findViewById(R.id.scheduled_room_date);
+//            headerTextView.setText(itemHeader);
+//
+//            int widthSpec = View.MeasureSpec.makeMeasureSpec(parent.getWidth(), View.MeasureSpec.EXACTLY);
+//            int heightSpec = View.MeasureSpec.makeMeasureSpec(parent.getHeight(), View.MeasureSpec.UNSPECIFIED);
+//            int childWidth = ViewGroup.getChildMeasureSpec(widthSpec,
+//                    parent.getPaddingLeft() + parent.getPaddingRight(),
+//                    headerView.getLayoutParams().width);
+//            int childHeight = ViewGroup.getChildMeasureSpec(heightSpec,
+//                    parent.getPaddingTop() + parent.getPaddingBottom(),
+//                    headerView.getLayoutParams().height);
+//            headerView.measure(childWidth, childHeight);
+//            headerView.layout(0, 0, headerView.getMeasuredWidth(), headerView.getMeasuredHeight());
+//            headerView.draw(c);
+//        }
     }
 
 }
